e877232a8d11088815daa4b498bc1332e64bb6d2
balakrishbalu <46368792+balakrishbalu@users.noreply.github.com>
2019-02-04 15:33:15 +0530

    Update largest_3.py

--- largest_3.py
+++ largest_3.py
@@ -1,9 +1,9 @@
 num1,num2,num3= [int(x) for x in raw_input().split(" ")] 
-if (num1 > num2) and (num1 > num3):
+if num1 > num2 and num1 > num3:
    largest = num1
-elif (num2 > num1) and (num2 > num3):
+elif num2 > num1 and num2 > num3:
    largest = num2
 else:
    largest = num3
  
-print(largest)
+print largest
