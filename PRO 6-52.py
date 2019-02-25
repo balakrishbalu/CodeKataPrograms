eb50b318c57e18683c315484382e02937fb2c546
balakrishbalu <46368792+balakrishbalu@users.noreply.github.com>
2019-02-25 15:49:57 +0530

    Create PRO 6-52.py

---
+++ PRO 6-52.py
@@ -0,0 +1,8 @@
+w1,w2=map(int,rawinput().split())
+x1,x2=map(int,rawinput().split())
+y1,y2=map(int,rawinput().split())
+z1,z2=map(int,rawinput().split())
+if(w1==x1 and y1==z1 and w2==x2 and y2==z2):
+  print "yes"
+else:
+  print "no"
