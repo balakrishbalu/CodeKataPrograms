cec3f706760d08290545fca0a0ac512da803fc6b
balakrishbalu <46368792+balakrishbalu@users.noreply.github.com>
2019-02-25 15:56:21 +0530

    Update PRO 6-52.py

--- PRO 6-52.py
+++ PRO 6-52.py
@@ -1,8 +1,8 @@
-w1,w2=map(int,rawinput().split())
-x1,x2=map(int,rawinput().split())
-y1,y2=map(int,rawinput().split())
-z1,z2=map(int,rawinput().split())
-if(w1==x1 and y1==z1 and w2==x2 and y2==z2):
+w1,w2=map(int,raw_input().split())
+x1,x2=map(int,raw_input().split())
+y1,y2=map(int,raw_input().split())
+z1,z2=map(int,raw_input().split())
+if(w1==x1 and y1==z1 and w2==z2 and y2==x2):
   print "yes"
 else:
   print "no"
